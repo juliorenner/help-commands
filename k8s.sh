a7b636fd360f32fa7e3a34bc392c4c5dba17592f
Julio Renner <julio@routific.com>
2023-01-30 22:42:55 -0300

    Update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -108,4 +108,4 @@ kubectl config use-context demouser@kubernetes-demo --kubeconfig=demouser.conf
 
 
 # Get all namespace secret values with kdecode
-kubectl get secrets -oname --no-headers | awk -F '/' '{print $2}' | xargs -I {} sh -c 'kdecode {}'
+kubectl get secrets -oname --no-headers | awk -F '/' '{print $2}' | xargs -I {} sh -c 'echo {}; kdecode {}'
